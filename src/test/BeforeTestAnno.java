04e04c02082df5c39dbfadc26885167001865558
UdaniSoo <isuru.udani1@gmail.com>
2018-11-13 15:29:24 +0800

    annotation group/bfmethod,bfclass,bef

--- src/test/BeforeTestAnno.java
+++ src/test/BeforeTestAnno.java
@@ -1,6 +1,12 @@
 package test;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -10,6 +16,25 @@ public class BeforeTestAnno {
 	{
 		System.out.println("Normal test");
 	}
+	
+	@BeforeClass
+	public void beforeclass()
+	{
+		System.out.println("This is before class methhod");
+	}
+	
+	@AfterClass
+	public void afterclass()
+	{
+		System.out.println("This is after class method");
+	}
+	
+	@Test
+	public void normaltest2()
+	{
+		System.out.println("Normal test2");
+	}
+	
 	@BeforeTest
 	public void beforetest()
 	{
@@ -20,6 +45,29 @@ public class BeforeTestAnno {
 	public void aftertest()
 	{
 		System.out.println("This is After test");
+	}
+	@BeforeSuite
+	public void beforeSuite()
+	{
+		System.out.println("This is before suite");
+	}
+	
+	@AfterSuite
+	public void afterSuite()
+	{
+		System.out.println("This is after suite");
+	}
+	
+	@BeforeMethod
+	public void beforeMethod()
+	{
+		System.out.println("This is before method");
+	}
+	
+	@AfterMethod
+	public void afterMethod()
+	{
+		System.out.println("This is after method");
 	}
 
 }
